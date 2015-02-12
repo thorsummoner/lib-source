12eb9b98a946976307b81022e2228825430cc47a
Dylan Grafmyre <thorsummoner@live.com>
2015-02-11 22:29:15 -0800

    Pylint for serverlist

--- libsource/serverlist.py
+++ libsource/serverlist.py
@@ -1,16 +1,24 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import server
+""" High level server list module.
+"""
+
+from libsource import server
 
 class ServerList(list):
+    """ Get server list.
+    """
     def __init__(self):
         super(ServerList, self).__init__([])
 
     def __repr__(self):
         return '<ServerList: {0}>'.format(len(self))
-        
+
     def append(self, item):
+        """Append server object to server list.
+        """
         if not isinstance(item, server.Server):
             raise TypeError('item must be from type Server')
-        if not any(map(lambda obj: obj == item, self)):
+        if not item in self:
             super(ServerList, self).append(item)
